8b107c13b3f9a3fa5b31e63a13f4af14c815a56b
elisabichiappini <elisabichiappini@gmail.com>
2024-02-01 18:32:00 +0100

    feat: migration table novels

---
+++ database/migrations/2024_02_01_164055_create_novels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('novels', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 300);
+            $table->text('description')->nullable();
+            $table->string('thum', 400);
+            $table->char('price', 7);
+            $table->string('series', 200);
+            $table->date('sale_time');
+            $table->string('type', 100);
+            $table->string('artist', 800);
+            $table->string('writer', 800);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('novels');
+    }
+};
