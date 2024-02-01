8b623b972f6ba4948b23052f3eab16d00b913a60
elisabichiappini <elisabichiappini@gmail.com>
2024-02-01 20:54:34 +0100

    feat: layout index

--- resources/views/novels/index.blade.php
+++ resources/views/novels/index.blade.php
@@ -5,11 +5,11 @@
     <div class="row row-cols-1 row-cols-md-3 g-4">
         @foreach ($novels as $item_novel )
         <div class="col">
-            <div class="card">
-                <img src="{{ $item_novel->thumb}}" class="card-img-top" alt="{{ $item_novel->title}}">
+            <div class="card text-center">
+                <img src="{{ $item_novel->thumb}}" class="card-img-top ec-img" alt="{{ $item_novel->title}}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $item_novel->type}}</h5>
-                    <p class="card-text">{{ $item_novel->description}}.</p>
+                    <p class="card-text">{{ $item_novel->series}}</p>
                 </div>
             </div>
         </div>
