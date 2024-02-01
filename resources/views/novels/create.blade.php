dd7506cd62f815ba4c923e0de0703a1b98ff6286
elisabichiappini <elisabichiappini@gmail.com>
2024-02-02 00:27:45 +0100

    feat: add form

--- resources/views/novels/create.blade.php
+++ resources/views/novels/create.blade.php
@@ -8,19 +8,74 @@
         <h2>New Novel</h2>
         <!--forum-->
         <form>
+            <!--title-->
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title">
             </div>
+            <!--/title-->
+            
+            <!--price-->
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="price" class="form-label">Price</label>
+                <input type="text" class="form-control" id="price" name="price">
             </div>
+            <!--/price-->
+
+            <!--series-->
+            <div class="mb-3">
+                <label for="series" class="form-label">Series</label>
+                <input type="text" class="form-control" id="series" name="series">
+            </div>
+            <!--series-->
+
+            <!--sale_date-->
+            <div class="mb-3">
+                <label for="sale_date" class="form-label">Sale Date</label>
+                <input type="text" class="form-control" id="sale_date" name="sale_date">
+            </div>
+            <!--/sale_date-->
+
+            <!--type-->
+            <div class="mb-3">
+                <label for="type" class="form-label">Type</label>
+                <input type="text" class="form-control" id="type" name="type">
+            </div>
+            <!--/type-->
+
+            <!--artists-->
+            <div class="mb-3">
+                <label for="artists" class="form-label">Artists</label>
+                <input type="text" class="form-control" id="artists" name="artists">
+            </div>
+            <!--/artists-->
+
+            <!--writers-->
+            <div class="mb-3">
+                <label for="writers" class="form-label">Writers</label>
+                <input type="text" class="form-control" id="writers" name="writers">
+            </div>
+            <!--/writers-->
+
+            <!--thumb-->
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Thumb</label>
+                <input type="text" class="form-control" id="thumb" name="thumb">
+            </div>
+            <!--/thumb-->
+
+            <!--description-->
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <input type="text-area" class="form-control" id="description" name="description">
+            </div>
+            <!--/description-->
+
             <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <label class="form-check-label" for="exampleCheck1"><em>Not a computer</em></label>
             </div>
+            
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
         <!--/forum-->
