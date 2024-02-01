0a69ac04257f0d7b89dd0af62e7b13588e75837c
elisabichiappini <elisabichiappini@gmail.com>
2024-02-01 20:47:35 +0100

    feat: add footer e header

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div id="bottom" class="bg-dark d-flex justify-content-center">
+        <span class="text-primary">Creating by Elisa ©</span>
+    </div>
+</footer>
---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <!--intestazione-->
+    <div class="d-flex justify-content-center p-4">
+        <h1 class="text-primary fw-bolder">DC Comics</h1>
+    </div>
+    <!--/intestazione-->
+    <!--navbar-->
+  
+    <nav class="d-flex justify-content-center navbar navbar-expand-lg">
+        <a class="navbar-brand" href="/">Home</a>
+        <a class="navbar-brand" href="#">Graphic Novels</a>
+        <a class="navbar-brand" href="#">New Novel</a>
+    </nav>
+
+    <!--/navbar-->
+</header>
