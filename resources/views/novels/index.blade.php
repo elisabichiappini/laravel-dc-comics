bc6848067388989c5e2f864390f0d2542d281b75
elisabichiappini <elisabichiappini@gmail.com>
2024-02-05 15:53:43 +0100

    feat: confirm delete record

--- resources/views/novels/index.blade.php
+++ resources/views/novels/index.blade.php
@@ -18,19 +18,53 @@
                     <!--link show novel-->
                     <a class="pb-1 nav-link ec-text" href="{{ route('novels.show', $item_novel->id) }}">More info</a>
                     <!--/link show novel-->
-                    <!--link show novel-->
+                    <!--link edit novel-->
                     <a class="pb-1 nav-link ec-text" href="{{ route('novels.edit', $item_novel->id) }}">Edit Novels</a>
-                    <!--/link show novel-->
-                    <!--delete record-->
-                    <form action="{{ route('novels.destroy', $item_novel->id) }}" method="POST">
-                        <!--token-->
-                        @csrf
-                        <!--/token-->
-                        @method('DELETE')
-                        <button class="btn btn-danger" type="submit">Delete</button>
-                    </form>
+                    <!--/link edit novel-->
+               
+                    <!--add confirm delete-->
+                    <div>
+                        <!--attiva modale di cancellazione-->
+                        <button class="btn btn-danger" type="submit" data-bs-toggle="modal" data-bs-target="#my-dialog-{{ $item_novel->id }}">Delete</button>
+                        <!--attiva modale di cancellazione-->
+
+                        <!--modale -->
+                        <div class="modal" id="my-dialog-{{ $item_novel->id }}">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <!--messaggio di alert -->
+                                    <div class="modal-header text-center">
+                                        <h3 class=text-center>Delete it?</h3>
+                                    </div>
+                                    <!--/messaggio di alert -->
+                                    <!--name novel to delete -->
+                                    <div class="modal-body text-center">
+                                        The novel <strong>{{ $item_novel->title }}</strong></span>
+                                    </div>
+                                    <!--/name novel to delete -->
+                                    <div class="modal-footer">
+                                        <!--no delete-->
+                                        <button class="btn btn-success text-uppercase"
+                                            data-bs-dismiss="modal">No
+                                        </button>
+                                        <!--no delete-->
+                                        <!--last confirm delete record-->
+                                        <form action="{{ route('novels.destroy', $item_novel->id) }}" method="POST">
+                                            <!--token-->
+                                            @csrf
+                                            <!--/token-->
+                                            @method('DELETE')
+                                            <button class="btn btn-danger" type="submit">Yes</button>       
+                                        </form>
+                                        <!--/last confirm delete record-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--modale -->
+                    </div>
+                    <!--card-->
                 </div>
-                <!--card-->
             </div>
             @endforeach
         </div>
