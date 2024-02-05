fe6622bf9d53af2a3e128df4562bc25f8befa139
elisabichiappini <elisabichiappini@gmail.com>
2024-02-06 00:05:34 +0100

    feat: layout more usable

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,23 +1,25 @@
 <!--header-->
-<header id="ec-nav" class="d-flex justify-content-between position-fixed top-0 end-0 start-0 z-index">
+<header id="ec-nav" class="d-flex justify-content-between align-items-center position-fixed top-0 end-0 start-0 z-index">
     <!--intestazione-->
     <div class="d-flex justify-content-center p-4">
         <h1 class="fw-bolder">DC Comics</h1>
     </div>
+    
+    <div>
+        <!--navbar-->
+        <nav class="d-flex justify-content-center navbar navbar-expand-lg gap-3">
+            <!--link go to home-->
+            <a class="navbar-brand text-white" href="{{ route('novel') }}">Home</a>
+            <!--/link go to home-->
+            <!--link list novels-->
+            <a class="navbar-brand text-white" href="{{ route('novels.index') }}">List Graphic Novels</a>
+            <!--/link list novels-->
+            <!--link add a novel-->
+            <a class="navbar-brand text-white" href="{{ route('novels.create') }}">Add Novel</a>
+            <!--/link add a novel-->
+        </nav>
+        <!--/navbar-->
+    </div>
     <!--/intestazione-->
-
-    <!--navbar-->
-    <nav class="d-flex justify-content-center navbar navbar-expand-lg gap-3">
-        <!--link go to home-->
-        <a class="navbar-brand text-white" href="{{ route('novel') }}">Home</a>
-        <!--/link go to home-->
-        <!--link list novels-->
-        <a class="navbar-brand text-white" href="{{ route('novels.index') }}">List Graphic Novels</a>
-        <!--/link list novels-->
-        <!--link add a novel-->
-        <a class="navbar-brand text-white" href="{{ route('novels.create') }}">Add Novel</a>
-        <!--/link add a novel-->
-    </nav>
-    <!--/navbar-->
 </header>
 <!--/header-->
