fc00d66cba210681c7b16c6c04e8186487fed775
elisabichiappini <elisabichiappini@gmail.com>
2024-02-07 10:30:09 +0100

    feat: favicon in website

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--add favicon-->
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico')}}">
+    <!--/add favicon-->
     <title>DC Comics</title>
     <!--inclusione assets con direttiva vite-->
     @vite('resources/js/app.js')
